bb2b1a460a9f62c4748816492b53b6376a58eadb
sthuarty <sthuarty@gmail.com>
2019-12-15 19:01:10 -0300

    Estruturando as lojas - Aula 180

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:loja_virtual/tabs/category_tab.dart';
 import 'package:loja_virtual/tabs/home_tab.dart';
 import 'package:loja_virtual/tabs/orders_tab.dart';
+import 'package:loja_virtual/tabs/places_tab.dart';
 import 'package:loja_virtual/widgets/cart_button.dart';
 import 'package:loja_virtual/widgets/custom_drawer.dart';
 
@@ -28,7 +29,14 @@ class HomeScreen extends StatelessWidget {
           body: CategoryTab(),
           floatingActionButton: CartButton(),
         ),
-        Container(color: Colors.yellow),
+        Scaffold(
+          appBar: AppBar(
+            title: Text("Lojas"),
+            centerTitle: true,
+          ),
+          body: PlacesTab(),
+          bottomNavigationBar: CustomDrawer(_pageController),
+        ),
         Scaffold(
           appBar: AppBar(title: Text("Meus pedidos"), centerTitle: true),
           body: OrdersTab(),
