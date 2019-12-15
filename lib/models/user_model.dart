fb27a07cc7f83ac3342cc46af584b75bfe1121b2
sthuarty <sthuarty@gmail.com>
2019-12-15 00:22:35 -0300

    Realizando o login - Aula 159

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -11,6 +11,13 @@ class UserModel extends Model {
 
   bool isLoading = false;
 
+  @override
+  void addListener(VoidCallback listener) {
+    super.addListener(listener);
+
+    _loadCurrentUser();
+  }
+
   void signUp(
       {@required Map<String, dynamic> userData,
       @required String pass,
@@ -64,14 +71,27 @@ Agora ela retorna um AuthResult e dentro dele pegamos o usuário no .user*/
     });
   }
 
-  void signIn() async {
+  void signIn(
+      {@required String email,
+      @required String pass,
+      @required VoidCallback onSuccess,
+      @required VoidCallback onFail}) async {
     isLoading = true;
     notifyListeners();
 
-    await Future.delayed(Duration(seconds: 3));
+    _auth.signInWithEmailAndPassword(email: email, password: pass).then((auth) async {
+      firebaseUser = auth.user;
 
-    isLoading = false;
-    notifyListeners();
+      await _loadCurrentUser();
+
+      onSuccess();
+      isLoading = false;
+      notifyListeners();
+    }).catchError((e) {
+      onFail();
+      isLoading = false;
+      notifyListeners();
+    });
   }
 
   void signOut() async {
@@ -95,5 +115,19 @@ Agora ela retorna um AuthResult e dentro dele pegamos o usuário no .user*/
         .collection("users")
         .document(firebaseUser.uid)
         .setData(userData);
+  }
+
+  Future<Null> _loadCurrentUser() async {
+    if (firebaseUser == null) firebaseUser = await _auth.currentUser();
+    if (firebaseUser != null) {
+      if (userData["name"] == null) {
+        DocumentSnapshot docUser = await Firestore.instance
+            .collection("users")
+            .document(firebaseUser.uid)
+            .get();
+        userData = docUser.data;
+      }
+    }
+    notifyListeners();
   }
 }
