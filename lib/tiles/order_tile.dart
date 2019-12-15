f31b3d9c834ef75a1ef22e49f928a693f3505711
sthuarty <sthuarty@gmail.com>
2019-12-15 18:42:10 -0300

    Acompanhando o status do pedido - Aula 179

--- lib/tiles/order_tile.dart
+++ lib/tiles/order_tile.dart
@@ -23,6 +23,8 @@ class OrderTile extends StatelessWidget {
                 if (!snapshot.hasData)
                   return Center(child: CircularProgressIndicator());
                 else {
+                  int status = snapshot.data["status"];
+
                   return Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
@@ -31,7 +33,25 @@ class OrderTile extends StatelessWidget {
                         style: TextStyle(fontWeight: FontWeight.bold),
                       ),
                       SizedBox(height: 4),
-                      Text(_buildProductsText(snapshot.data))
+                      Text(_buildProductsText(snapshot.data)),
+                      SizedBox(height: 4),
+                      Text(
+                        "Status do pedido:",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 4),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          _buildCircle("1", "Preparação", status, 1),
+                          Container(
+                              height: 1, width: 40, color: Colors.grey[500]),
+                          _buildCircle("2", "Transporte", status, 2),
+                          Container(
+                              height: 1, width: 40, color: Colors.grey[500]),
+                          _buildCircle("3", "Entrega", status, 3)
+                        ],
+                      ),
                     ],
                   );
                 }
@@ -49,5 +69,37 @@ class OrderTile extends StatelessWidget {
     text += "Total: R\$ ${snapshot.data["totalPrice"].toStringAsFixed(2)}";
 
     return text;
+  }
+
+  Widget _buildCircle(
+      String title, String subTitle, int status, int thisStatus) {
+    Color backColor;
+    Widget child;
+
+    if (status < thisStatus) {
+      backColor = Colors.grey[500];
+      child = Text(title, style: TextStyle(color: Colors.white));
+    } else if (status == thisStatus) {
+      backColor = Colors.blue;
+      child = Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Text(title, style: TextStyle(color: Colors.white)),
+          CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+          )
+        ],
+      );
+    } else {
+      backColor = Colors.green;
+      child = Icon(Icons.check, color: Colors.white);
+    }
+
+    return Column(
+      children: <Widget>[
+        CircleAvatar(radius: 20, backgroundColor: backColor, child: child),
+        Text(subTitle)
+      ],
+    );
   }
 }
