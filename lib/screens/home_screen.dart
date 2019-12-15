ca8c899419182a4f0d801069c84f030dcc9c0e8b
sthuarty <sthuarty@gmail.com>
2019-12-15 17:47:55 -0300

    Criando a tab de pedidos - Aula 176

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/tabs/category_tab.dart';
 import 'package:loja_virtual/tabs/home_tab.dart';
+import 'package:loja_virtual/tabs/orders_tab.dart';
 import 'package:loja_virtual/widgets/cart_button.dart';
 import 'package:loja_virtual/widgets/custom_drawer.dart';
 
@@ -26,6 +27,12 @@ class HomeScreen extends StatelessWidget {
           drawer: CustomDrawer(_pageController),
           body: CategoryTab(),
           floatingActionButton: CartButton(),
+        ),
+        Container(color: Colors.yellow),
+        Scaffold(
+          appBar: AppBar(title: Text("Meus pedidos"), centerTitle: true),
+          body: OrdersTab(),
+          drawer: CustomDrawer(_pageController),
         ),
       ],
     );
