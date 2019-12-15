af524f9471a0b58df4ef66f2775f034b17410a91
sthuarty <sthuarty@gmail.com>
2019-12-15 16:37:46 -0300

    Criando a card do frete - Aula 170

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -4,6 +4,7 @@ import 'package:loja_virtual/models/user_model.dart';
 import 'package:loja_virtual/screens/login_screen.dart';
 import 'package:loja_virtual/tiles/cart_tile.dart';
 import 'package:loja_virtual/widgets/discount_cart.dart';
+import 'package:loja_virtual/widgets/ship_card.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class CartScreen extends StatelessWidget {
@@ -86,6 +87,7 @@ class CartScreen extends StatelessWidget {
                   }).toList(),
                 ),
                 DiscountCart(),
+                ShipCard(),
               ],
             );
           }
